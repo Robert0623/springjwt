76c52b40e4120f1aa1bf02afe66294a2ea04085c
hwoo <tone8943@gmail.com>
2025-05-22 19:38:36 +0900

    Initial commit

---
+++ src/main/java/com/cos/springjwt/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.cos.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+
+        return "Admin Controller";
+    }
+}
